c321eab32d60e32bc490365e0a728e8a7cefd6e1
yu <ybw2016v@126.com>
2021-11-13 15:09:29 +0800

    update

---
+++ packages/client/src/components/image-viewer.vue
@@ -0,0 +1,120 @@
+<template>
+<MkModal ref="modal" @click="$refs.modal.close()" @closed="$emit('closed')">
+	<div class="xubzgfga">
+		<header>{{ image.name }}</header>
+		<img :src="image.url" :alt="image.comment" :title="image.comment" @click="$refs.modal.close()"/>
+		<footer>
+			<span>{{ image.type }}</span>
+			<span>{{ bytes(image.size) }}</span>
+			<span v-if="image.properties && image.properties.width">{{ number(image.properties.width) }}px × {{ number(image.properties.height) }}px</span>
+		</footer>
+	</div>
+</MkModal>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+<<<<<<< HEAD:src/client/components/image-viewer.vue
+import bytes from '@client/filters/bytes';
+import number from '@client/filters/number';
+import MkModal from '@client/components/ui/modal.vue';
+import 'viewerjs/dist/viewer.css';
+import Viewer from 'viewerjs';
+=======
+import bytes from '@/filters/bytes';
+import number from '@/filters/number';
+import MkModal from '@/components/ui/modal.vue';
+
+>>>>>>> 2795fe457909c687f668d020ef65d52abc3182fb:packages/client/src/components/image-viewer.vue
+export default defineComponent({
+	components: {
+		MkModal,
+	},
+	props: {
+		image: {
+			type: Object,
+			required: true
+		},
+	},
+	emits: ['closed'],
+	mounted ()
+	{
+		const ua = navigator.userAgent.toLowerCase();
+		const isiOS =  /ipod|iphone|ipad/.test(ua);
+		if (!isiOS) {
+			this.createViewer();
+		}
+	},
+	destroyed (){
+		this.destroyViewer();
+	},
+	methods: {
+		bytes,
+		number,
+		destroyViewer () {
+			this.$viewer && this.$viewer.destroy()
+		},
+		createViewer () {
+			let title=(this.image.properties && this.image.properties.width)?`${this.image.type} | ${bytes(this.image.size)} | ${this.image.comment} (${number(this.image.properties.width)}px × ${number(this.image.properties.height)}px)`:`${this.image.type} | ${bytes(this.image.size)} | ${this.image.comment}`;
+			let toolbar={
+				zoomIn: 4,
+				zoomOut: 4,
+				oneToOne: 4,
+				reset: 4,
+				prev: 0,
+				play: {
+					show: 4,
+					size: 'large',
+				},
+				next: 0,
+				rotateLeft: 4,
+				rotateRight: 4,
+				flipHorizontal: 4,
+				flipVertical: 4,};
+			this.$viewer = new Viewer(this.$el, {title: [1, ()=>title],navbar:0,toolbar:toolbar,slideOnTouch:false,zIndex:99999});
+			this.$viewer.show();
+			this.$refs.modal.close(); //Fix the bug that one same image show twice
+		}
+		}
+});
+</script>
+
+<style lang="scss" scoped>
+.xubzgfga {
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	> header,
+	> footer {
+		align-self: center;
+		display: inline-block;
+		padding: 6px 9px;
+		font-size: 90%;
+		background: rgba(0, 0, 0, 0.5);
+		border-radius: 6px;
+		color: #fff;
+	}
+	> header {
+		margin-bottom: 8px;
+		opacity: 0.9;
+	}
+	> img {
+		display: block;
+		flex: 1;
+		min-height: 0;
+		object-fit: contain;
+		width: 100%;
+		cursor: zoom-out;
+		image-orientation: from-image;
+	}
+	> footer {
+		margin-top: 8px;
+		opacity: 0.8;
+		> span + span {
+			margin-left: 0.5em;
+			padding-left: 0.5em;
+			border-left: solid 1px rgba(255, 255, 255, 0.5);
+		}
+	}
+}
+</style>
