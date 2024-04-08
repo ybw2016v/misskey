d3f81d156646bb09385e249d7356624178c241f9
dogcraft <neko@neko.red>
2024-04-08 16:30:21 +0800

    pnpm

--- packages/frontend/src/pages/settings/migration.vue
+++ packages/frontend/src/pages/settings/migration.vue
@@ -21,7 +21,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 			<div class="_gaps">
 				<MkInput v-for="(_, i) in accountAliases" v-model="accountAliases[i]">
 					<template #prefix><i class="ti ti-plane-arrival"></i></template>
-					<template #label>{{ i18n.tsx._accountMigration.moveFromLabel({ n: i + 1 }) }}</template>
+					<!-- <template #label>{{ i18n.tsx._accountMigration.moveFromLabel({ n: i + 1 }) }}</template> -->
 				</MkInput>
 			</div>
 		</div>
